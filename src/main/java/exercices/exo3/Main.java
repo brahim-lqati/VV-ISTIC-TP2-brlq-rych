86bc05fd4222a2b50d676b5ab66b378060e92351
blqati <brahim98lqati@gmail.com>
2023-03-03 20:54:55 +0100

    answer exo3 - No getter

---
+++ src/main/java/exercices/exo3/Main.java
@@ -0,0 +1,42 @@
+package exercices.exo3;
+
+import com.github.javaparser.Problem;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.utils.SourceRoot;
+import exercices.exo3.No_Getter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("args " + args[0]);
+        if(args.length == 0) {
+            System.err.println("Should provide the path to the source code");
+            System.exit(1);
+        }
+
+        File file = new File(args[0]);
+        System.out.println(file);
+        if(!file.exists() || !file.isDirectory() || !file.canRead()) {
+            System.err.println("Provide a path to an existing readable directory");
+            System.exit(2);
+        }
+
+        SourceRoot root = new SourceRoot(file.toPath());
+        No_Getter printer = new No_Getter();
+        root.parse("", (localPath, absolutePath, result) -> {
+            result.ifSuccessful(unit -> unit.accept(printer, null));
+            return SourceRoot.Callback.Result.DONT_SAVE;
+        });
+    }
+
+
+}
---
+++ src/main/java/exercices/exo3/No_Getter.java
@@ -0,0 +1,64 @@
+package exercices.exo3;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class No_Getter extends VoidVisitorWithDefaults<Void> {
+    private final FileOutputStream report = new FileOutputStream(new File("report.txt"));
+
+    private String currentPackge = "";
+
+    public No_Getter() throws IOException {
+        report.write("Private field | Class Name | Package".getBytes(StandardCharsets.UTF_8));
+        report.write("\n----------------------------------".getBytes());
+    }
+
+    @Override
+    public void visit(CompilationUnit unit, Void arg) {
+        // get Package Name
+        unit.getPackageDeclaration().ifPresent(pack -> currentPackge = pack.getNameAsString());
+
+        for(ClassOrInterfaceDeclaration type : unit.findAll(ClassOrInterfaceDeclaration.class)) {
+            type.accept(this, null);
+        }
+    }
+
+    @Override
+    public void visit(ClassOrInterfaceDeclaration declaration, Void arg) {
+        for (FieldDeclaration field: declaration.getFields()) {
+            if (field.isPrivate()) {
+                field.getVariables().forEach(v -> {
+                    String nameField = v.getNameAsString();
+                    String nameG = "get" + nameField.substring(0,1).toUpperCase() +
+                           nameField.substring(1);
+
+                    List<MethodDeclaration> getter = declaration.getMethodsByName(nameG);
+                    if (getter == null ||
+                            getter.size() == 0 ||
+                            getter.get(0).isPrivate() ||
+                            !getter.get(0).getTypeAsString().equals(v.getTypeAsString())
+                    ) {
+                        // found issue => write to file
+                        // new line
+                        String line = String.format("\n%s | %s | %s", nameField, declaration.getNameAsString(), currentPackge);
+                        try {
+                            report.write(line.getBytes());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+            }
+        }
+
+    }
+}
